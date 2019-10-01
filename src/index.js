ec2113edc2b7786f578eb24ec678a10fe57e8ea0
ingrid <msagrerom@inee.edu.mx>
2019-10-01 11:01:25 -0500

    reto completo, incluido el problema 4

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
+
+localStorage.removeItem('next_fetch');
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+
+      localStorage.setItem('next_fetch', response.info.next)
+
       const characters = response.results;
       let output = characters.map(character => {
         return `
@@ -23,16 +29,28 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+async function loadData() {
+  const nextUrl = localStorage.getItem('next_fetch');
+  if(nextUrl === null){
+    await getData(API);
+  } else if (nextUrl === '') {
+      let notification = document.createElement('h1')
+      let text = document.createTextNode('Ya no hay personajes :(')
+      notification.appendChild(text)
+      $app.appendChild(notification);
+      intersectionObserver.unobserve($observe);
+  } else {
+    await getData (nextUrl);
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+  
+} 
 
-intersectionObserver.observe($observe);
+  const intersectionObserver = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  }, {
+    rootMargin: '0px 0px 100% 0px',
+  });
+  
+  intersectionObserver.observe($observe);
